24137f423e87aeb1aa2d7d61ceee6c666d080a4b
Michal Jeřábek <mjerabek@seznam.cz>
2016-02-29 18:51:22 +0100

    sekce se přeřazují animovaně

--- client/page/index.js
+++ client/page/index.js
@@ -1,5 +1,4 @@
 /*jslint indent: 4, white: true, nomen: true, regexp: true, unparam: true, node: true, browser: true, devel: true, nomen: true, plusplus: true, regexp: true, sloppy: true, vars: true*/
-/*global $*/
 
 var Ractive = require("ractive");
 
